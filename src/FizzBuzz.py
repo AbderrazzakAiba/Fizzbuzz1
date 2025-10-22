ccf99068aaa0aaa24a4c88577b27fb0bf857fc6d
Abderrazzak Aiba <aibaabderrazzak7@gmail.com>
2025-10-22 16:41:16 +0100

    test 3 is fizz

--- src/FizzBuzz.py
+++ src/FizzBuzz.py
@@ -1,6 +1,9 @@
 def fizzbuzz(nombre):
     if (nombre % 3 == 0) and (nombre % 5 == 0):
         return "fizzbuzz"
+    if (nombre %3 ==0):
+        return "fizz"
     if (nombre %3 !=0) and (nombre %5!=0) :
         return nombre
     return "fizzbuzz"
+
--- test/test_fizzbuzz.py
+++ test/test_fizzbuzz.py
@@ -14,6 +14,7 @@ from src.FizzBuzz import fizzbuzz
 def test_numbers_return_expected(nombre, expected_result):
     actual_result = fizzbuzz(nombre)
     assert actual_result == expected_result
-
+def test_3_is_fizz():
+    assert fizzbuzz(3)=="fizz"
 
 
