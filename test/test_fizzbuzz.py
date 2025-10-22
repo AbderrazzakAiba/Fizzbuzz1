c187160647a47d477c3ba6515150cdc8a8edf1ee
Abderrazzak Aiba <aibaabderrazzak7@gmail.com>
2025-10-22 11:36:34 +0100

    refactor

--- test/test_fizzbuzz.py
+++ test/test_fizzbuzz.py
@@ -1,10 +1,15 @@
-
+import pytest
 from src.FizzBuzz import fizzbuzz
+@pytest.mark.parametrize(
+    "nombre, expected_result",
+    [
+        (0, "fizzbuzz"),
+        (1, 1),
+        (2,2),
+    ]
+)
+def test_numbers_return_expected(nombre, expected_result):
+    actual_result = fizzbuzz(nombre)
+    assert actual_result == expected_result
 
 
-def test_0_is_fizzbuzz():
-    assert fizzbuzz(0)=="fizzbuzz"
-def test_1_is_not_fizzbuzz():
-    assert fizzbuzz(1)==1
-def test_2_is_not_fizzbuzz():
-    assert fizzbuzz(2)==2
